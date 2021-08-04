bd05d1a0556586434acd12627929c17976404381
User <User@Admin>
2021-08-04 09:38:27 +0530

    FirstPush

---
+++ com.Demo/src/test/java/com/DemoTest/RegressionCaseTest.java
@@ -0,0 +1,11 @@
+package com.DemoTest;
+
+import org.testng.annotations.Test;
+
+public class RegressionCaseTest {
+@Test
+public void Regression()
+{
+	System.out.println("Hello this is Regression Testing");
+}
+}
---
+++ com.Demo/src/test/java/com/DemoTest/SystemCaseTest.java
@@ -0,0 +1,11 @@
+package com.DemoTest;
+
+import org.testng.annotations.Test;
+
+public class SystemCaseTest {
+@Test
+public void smokeTest()
+{
+	System.out.println("Hello this is Smoke Testing");
+}
+}
